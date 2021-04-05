26a3f343d17ab43c2a83589869fa48912ac47466
eehwan <eehwan.shin@gmail.com>
2021-04-05 23:47:08 +0900

    edit /polls page

--- testProject/polls/views.py
+++ testProject/polls/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
-# Create your views here.
-def index(req):
-    return HttpResponse("Hello World. You are at the polls index.")
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = "<div>Hello World. You are at the polls index.<div>"
+    for question in latest_question_list:
+        output += f"<a href='./{question.id}'>{question.question_text}</a>"
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}. <a href='./results'>Results</a> <a href='./vote'>Vote</a>")
