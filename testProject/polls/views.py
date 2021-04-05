bac16d40086d6da5c48dbc920aaa38ade71f8f37
eehwan <eehwan.shin@gmail.com>
2021-04-05 23:40:53 +0900

    make /polls/detail pages

--- testProject/polls/views.py
+++ testProject/polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(req):
     return HttpResponse("Hello World. You are at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}. <a href='./results'>Results</a> <a href='./vote'>Vote</a>")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
