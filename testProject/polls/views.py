f3c930da78de0dd79134a6917b4e41800a177452
eehwan <eehwan.shin@gmail.com>
2021-04-07 22:14:27 +0900

    update views.py of '/polls' & make detail.html

--- testProject/polls/views.py
+++ testProject/polls/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.http import Http404
+# from django.http import Http404
 
-from .models import Question
-
+from .models import Question, Choice
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:20]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/index.html', { "latest_question_list": latest_question_list })
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}. <a href='./results'>Results</a> <a href='./vote'>Vote</a>")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { "question": question })
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # reverse는 url을 하드코딩하지 않기 위해 사용
